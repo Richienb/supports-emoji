4182a6298441c25d7e9e0e99d1b523b9329194cd
Richie Bendall <richiebendall@gmail.com>
2020-01-01 21:38:14 +1300

    feat: Update code
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 "use strict"
 
-module.exports = (input, { postfix = "rainbows" } = {}) => {
-    if (typeof input !== "string") throw new TypeError(`Expected a string, got ${typeof input}`)
+const hasFlag = require("has-flag")
+const platform = require("os").platform()
+const execa = require("execa")
 
-    return `${input} & ${postfix}`
+const { env } = process
+
+module.exports = () => {
+    if (hasFlag("no-emoji") ||
+        hasFlag("no-emojis") ||
+        hasFlag("emoji=false") ||
+        hasFlag("emoji=never")) {
+        return 0
+    } else if (hasFlag("emoji") ||
+        hasFlag("emojis") ||
+        hasFlag("emoji=true") ||
+        hasFlag("emoji=always")) {
+        return 1
+    }
+
+    if ("FORCE_EMOJI" in env) {
+        if (env.FORCE_EMOJI === "true") {
+            return 1
+        } else if (env.FORCE_EMOJI === "false") {
+            return 0
+        } else {
+            return env.FORCE_EMOJI.length === 0 ? 1 : Math.min(parseInt(env.FORCE_EMOJI, 10), 3)
+        }
+    }
+
+    if (platform === "win32") {
+        if ("PsModulePath" in env) return 0
+        if ("ComSpec" in env && env.ComSpec.includes("cmd.exe")) return 2
+        return 0
+    } else if (platform === "darwin") {
+
+    } else if (platform === "linux") {
+        const val = execa.commandSync("echo \\u00E0\\u00A5\\u00A5 | wc -m")
+        console.log(val)
+    }
 }
